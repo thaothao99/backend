ebadd18451abbfcd1b3d57c4eca65d7f30d99238
thaothao99 <17521069@gm.uit.edu.vn>
2019-09-30 15:20:00 +0700

    2 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+      debug: false,
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: "localhost",
+      port: 27017,
+      database: "test",
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      useNewUrlParser: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
